25e8e662c297da49e79bd08de904f370ebafa743
Ricardo <humanoastuto321@gmail.com>
2020-06-09 00:50:11 -0400

    ESlint format

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,7 +12,7 @@
           <option v-for="(registry, index) in registrys" :key="index">
             {{ registry.name }}
           </option>
-      </select>
+        </select>
         <label>Transfer to: </label>
         <select
           class="browser-default custom-select"
@@ -21,74 +21,103 @@
           <option v-for="(registry, index) in registrys" :key="index">
             {{ registry.name }}
           </option>
-      </select>
+        </select>
         <label>Amount</label>
-        <input class="form-control" v-model="registry.transferAmount">
-        <button class="btn btn-primary" @click="transferRegistrySource(); transferRegistryDestination();">Transfer</button>
+        <input class="form-control" v-model="registry.transferAmount" />
+        <button
+          class="btn btn-primary"
+          @click="
+            transferRegistrySource();
+            transferRegistryDestination();
+          "
+        >
+          Transfer
+        </button>
       </div>
-
-
-
       <div class="form-group">
         <label>Name</label>
-        <input class="form-control" type="text" v-model="registry.name">
+        <input class="form-control" type="text" v-model="registry.name" />
       </div>
       <div class="form-group">
         <label>Category</label>
-        <input class="form-control" v-model="registry.category">
+        <input class="form-control" v-model="registry.category" />
       </div>
       <label>Type</label>
-      <select class="browser-default custom-select" v-model="registry.type_search">
-        <option v-for="(type, index) in typelist" :key="index"> {{ type.name }} </option>
+      <select
+        class="browser-default custom-select"
+        v-model="registry.type_search"
+      >
+        <option v-for="(type, index) in typelist" :key="index">
+          {{ type.name }}
+        </option>
       </select>
-       <div class="form-group">
+      <div class="form-group">
         <label>Amount</label>
-        <input class="form-control" v-model="registry.amount">
+        <input class="form-control" v-model="registry.amount" />
       </div>
-      <button class="btn btn-primary" @click="addRegistry">Add</button>        
+      <button class="btn btn-primary" @click="addRegistry">Add</button>
     </div>
     <div class="col-sm-12">
-      <div class="col-sm-8 nota" v-for="(registry, index) in registrys" :key="index">
-        <div :class="[registry.type_search ==='Income' ? 'card-income' : 'card-expense']" @click="prevUpdate(index)">
+      <div
+        class="col-sm-8 nota"
+        v-for="(registry, index) in registrys"
+        :key="index"
+      >
+        <div
+          :class="[
+            registry.type_search === 'Income' ? 'card-income' : 'card-expense'
+          ]"
+          @click="prevUpdate(index)"
+        >
           <div class="card-block">
-            <div class="card-title">{{registry.name}}</div>
-            <div class="card-subtitle mb-2 text-muted">{{registry.category}}</div>
-            <div class="card-subtitle mb-2 text-muted">{{registry.type_search}}</div>
-            <p class="card-text">{{registry.amount}}</p>
+            <div class="card-title">
+              {{ registry.name }}
+            </div>
+            <div class="card-subtitle mb-2 text-muted">
+              {{ registry.category }}
+            </div>
+            <div class="card-subtitle mb-2 text-muted">
+              {{ registry.type_search }}
+            </div>
+            <p class="card-text">
+              {{ registry.amount }}
+            </p>
           </div>
-          <button class="btn btn-primary" @click="updateRegistry(index)">Update</button>  
-          <button class="close" @click="delRegistry(index)">&times;</button>          
+          <button class="btn btn-primary" @click="updateRegistry(index)">
+            Update
+          </button>
+          <button class="close" @click="delRegistry(index)">&times;</button>
         </div>
       </div>
-    </div>        
-  </div>  
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'About',
-  data () {
+  name: "About",
+  data() {
     return {
-      title: 'Registry of transaction',
+      title: "Registry of transaction",
       typelist: [
-       {
-          name: 'Income'
-       },
-       {
-          name:'Expense'
-       }
-     ],
-      registry:{
-        name:'',
-        category:'',
-        amount:'',
-        type_search:'Income'
+        {
+          name: "Income"
+        },
+        {
+          name: "Expense"
+        }
+      ],
+      registry: {
+        name: "",
+        category: "",
+        amount: "",
+        type_search: "Income"
       },
       registrys: []
-    }
+    };
   },
-  methods:{
-    addRegistry:function(){
+  methods: {
+    addRegistry: function() {
       let { name, category, amount, type_search } = this.registry;
       this.registrys.push({
         name,
@@ -96,27 +125,26 @@ export default {
         amount,
         type_search
       });
-      localStorage.setItem('reg-local', JSON.stringify(this.registrys));
+      localStorage.setItem("reg-local", JSON.stringify(this.registrys));
     },
     delRegistry: function(index) {
       this.registrys.splice(index, 1);
-            localStorage.setItem('reg-local', JSON.stringify(this.registrys));
-
+      localStorage.setItem("reg-local", JSON.stringify(this.registrys));
     },
-    updateRegistry:function(index){
+    updateRegistry: function(index) {
       this.registrys[index].name = this.registry.name;
       this.registrys[index].category = this.registry.category;
       this.registrys[index].type_search = this.registry.type_search;
-      this.registrys[index].amount = this.registry.amount; 
-      localStorage.setItem('reg-local', JSON.stringify(this.registrys));
+      this.registrys[index].amount = this.registry.amount;
+      localStorage.setItem("reg-local", JSON.stringify(this.registrys));
     },
-    prevUpdate:function(index){
+    prevUpdate: function(index) {
       this.registry.name = this.registrys[index].name;
       this.registry.category = this.registrys[index].category;
       this.registry.type_search = this.registrys[index].type_search;
-      this.registry.amount = this.registrys[index].amount; 
+      this.registry.amount = this.registrys[index].amount;
     },
-     transfershow: function() {
+    transfershow: function() {
       var x = document.getElementById("myDIV");
       if (x.style.display === "none") {
         x.style.display = "block";
@@ -136,8 +164,7 @@ export default {
         amount,
         type_search
       });
-
-  },
+    },
     transferRegistryDestination: function() {
       let name = this.registry.transferDestination;
       let category = "Transfer";
@@ -153,40 +180,38 @@ export default {
     }
   },
 
-  created: function(){
-    let registrysDB = JSON.parse(localStorage.getItem('reg-local'));
+  created: function() {
+    let registrysDB = JSON.parse(localStorage.getItem("reg-local"));
     if (registrysDB === null) {
       this.registrys = [];
     } else {
       this.registrys = registrysDB;
     }
   }
-}
-
-
+};
 </script>
 
 <style>
-  .form{
-    text-align: left;
-  } 
-  .card-income{
-    background: lightgreen;
-    text-align: left;
-    border:1px solid #2c3e50;
-    border-radius: 4px;
-    padding-left: 8px;
-    padding-right: 8px;
-  } 
-  .card-expense{
-    background: lightcoral;
-    text-align: left;
-    border:1px solid #2c3e50;
-    border-radius: 4px;
-    padding-left: 8px;
-    padding-right: 8px;
-  } 
-  .nota{
-    padding: 5px;
-  }
+.form {
+  text-align: left;
+}
+.card-income {
+  background: lightgreen;
+  text-align: left;
+  border: 1px solid #2c3e50;
+  border-radius: 4px;
+  padding-left: 8px;
+  padding-right: 8px;
+}
+.card-expense {
+  background: lightcoral;
+  text-align: left;
+  border: 1px solid #2c3e50;
+  border-radius: 4px;
+  padding-left: 8px;
+  padding-right: 8px;
+}
+.nota {
+  padding: 5px;
+}
 </style>

c6ec236c86b08d28f09d30f3275028388e25189e
anditavillarroel99 <56407328+anditavillarroel99@users.noreply.github.com>
2020-06-11 05:37:04 -0400

    Revert "Task1"

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/categories">Categories</router-link>
+      <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
@@ -16,7 +15,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 20px;
 }
 
 #nav {
